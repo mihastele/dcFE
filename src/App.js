2f953119cab38c9de18f0f572a5343876ff52b78
Miha <stele1000@gmail.com>
2024-02-03 20:26:22 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,213 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import { z } from 'zod';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [stores, setStores] = useState([]);
+    const [storeAddress, setStoreAddress] = useState('');
+    const [storePhoneNumber, setStorePhoneNumber] = useState('');
+    const [servedBy, setServedBy] = useState('');
+    const [email, setEmail] = useState('');
+    const [btBroadband, setBtBroadband] = useState('');
+    const [btTvPackage, setBtTvPackage] = useState('');
+    const [sportPackage, setSportPackage] = useState('');
+    const [monthlyCharge, setMonthlyCharge] = useState('');
+    const [upfrontFee, setUpfrontFee] = useState('');
+    const [installments, setInstallments] = useState('');
+    const [otherInfo, setOtherInfo] = useState('');
+
+    const currentDate = new Date();
+    const formattedDate = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')}`;
+
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/feature/stores')
+            .then(response => response.json())
+            .then(data => {
+                setStores(data)
+                setStoreAddress(data[0].storeAddress);
+                setStorePhoneNumber(data[0].storePhoneNumber);
+            }).catch(error => console.error(error));
+    }, []);
+
+
+    const onSelected = (e) => {
+        fetch(`http://localhost:8080/api/feature/store/${e.target.value}`).then(response => response.json())
+            .then(data => {
+                setStoreAddress(data.storeAddress);
+                setStorePhoneNumber(data.storePhoneNumber);
+            }).catch(error => console.error(error));
+    }
+
+    const resetFields = () => {
+        setServedBy('');
+        setEmail('');
+        setBtBroadband('');
+        setBtTvPackage('');
+        setSportPackage('');
+        setMonthlyCharge('');
+        setUpfrontFee('');
+        setInstallments('');
+        setOtherInfo('');
+    }
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        const dataSchema = z.object({
+            storeId: z.string(),
+            date: z.date(),
+            servedBy: z.string(),
+            email: z.string().email(),
+            btBroadband: z.string(),
+            BTTVPackage: z.string(),
+            sportPackage: z.date(),
+            monthlyCharge: z.number().positive(),
+            upfrontFee: z.number().positive(),
+            installmentsOfPayment: z.number().max(5), // Ensure that installmentsOfPayment is 5 or less
+            otherHandyInfo: z.string().max(50), // Assuming otherHandyInfo should be 50 characters or less
+        });
+
+        const data = {
+            storeId: document.querySelector('select').value,
+            date: currentDate,
+            servedBy,
+            email,
+            btBroadband: btBroadband,
+            BTTVPackage: btTvPackage,
+            sportPackage: new Date(sportPackage),
+            monthlyCharge: +monthlyCharge,
+            upfrontFee: +upfrontFee,
+            installmentsOfPayment: +installments,
+            otherHandyInfo: otherInfo
+        }
+
+        console.log(data)
+        console.log(JSON.stringify(data));
+        // return;
+
+        const validationResult = dataSchema.safeParse(data);
+
+        if (validationResult.success) {
+            console.log('Data is valid!');
+        } else {
+            console.log('Data is invalid:', validationResult.error);
+            return;
+        }
+
+        fetch('http://localhost:8080/api/feature/dww-entry', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                resetFields();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    return (
+        <div className="App">
+
+            <form onSubmit={submitForm}>
+                <div className="d-flex py-2 px-5">
+                    <div className="d-flex w-50 flex-column">
+                        <label className="text-start">Store name</label>
+                        <select onChange={onSelected}>
+                            {
+                                stores.map(store => <option key={store.storeName} value={store.storeName}
+
+                                >{store.storeName}</option>)
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className="d-flex px-5">
+                    <div className="d-flex w-100">
+                        <div className="w-33 d-flex flex-column pe-2 py-2">
+                            <label className="text-start">Date</label>
+                            <input disabled type="date" value={formattedDate}/>
+                        </div>
+                        <div className="w-33 d-flex flex-column px-2 py-2">
+                            <label className="text-start">Store Address</label>
+                            <input disabled value={storeAddress}/>
+                        </div>
+                        <div className="w-33 d-flex flex-column ps-2 py-2">
+                            <label className="text-start">Store Phone Number</label>
+                            <input disabled value={storePhoneNumber}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex px-5">
+                    <div className="d-flex w-100">
+                        <div className="w-33 d-flex flex-column pe-2 py-2">
+                            <label className="text-start">Served By</label>
+                            <input value={servedBy} onChange={(e) => setServedBy(e.target.value)}/>
+                        </div>
+                        <div className="w-33 d-flex flex-column px-2 py-2">
+                            <label className="text-start">Email</label>
+                            <input value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+                        <div className="w-33 d-flex flex-column ps-2 py-2">
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex px-5">
+                    <div className="d-flex w-100">
+                        <div className="w-33 d-flex flex-column pe-2 py-2">
+                            <label className="text-start">BT Broadband is</label>
+                            <input value={btBroadband} onChange={(e) => setBtBroadband(e.target.value)}/>
+                        </div>
+                        <div className="w-33 d-flex flex-column px-2 py-2">
+                            <label className="text-start">BT TV package is</label>
+                            <input value={btTvPackage} onChange={(e) => setBtTvPackage(e.target.value)}/>
+                        </div>
+                        <div className="w-33 d-flex flex-column ps-2 py-2">
+                            <label className="text-start">Sport package is</label>
+                            <input type="date"  value={sportPackage} onChange={(e) => setSportPackage(e.target.value)}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex px-5">
+                    <div className="d-flex w-100">
+                        <div className="w-33 d-flex flex-column pe-2 py-2">
+                            <label className="text-start">Monthly charge</label>
+                            <input placeholder="£" value={monthlyCharge} onChange={(e) => setMonthlyCharge(e.target.value)}/>
+                        </div>
+                        <div className="w-33 d-flex flex-column px-2 py-2">
+                            <label className="text-start">Upfront fee</label>
+                            <input placeholder="£" value={upfrontFee} onChange={(e) => setUpfrontFee(e.target.value)}/>
+                        </div>
+                        <div className="w-33 d-flex flex-column ps-2 py-2">
+                            <label className="text-start">Installments of payments</label>
+                            <select  value={installments} onChange={(e) => setInstallments(e.target.value)}>
+                                <option value={1}>1</option>
+                                <option value={2}>2</option>
+                                <option value={3}>3</option>
+                                <option value={4}>4</option>
+                                <option value={5}>5</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex py-2 px-5">
+                    <div className="d-flex w-50 flex-column">
+                        <label className="text-start">Other handy information</label>
+                        <input maxLength="50" value={otherInfo} onChange={(e) => setOtherInfo(e.target.value)}/>
+                    </div>
+                </div>
+                <input onClick={resetFields} type="button" value="Reset"/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
